e5d19c7b3bcda553aa0726f5e7266e9867ea4d91
ywenli <kuota0204@gmail.com>
2020-07-15 00:44:43 +0900

    Add new and create actions in list Controller #9

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,2 +1,19 @@
 class ListController < ApplicationController
+
+  def new
+    @list = List.new
+  end
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to :root
+    else
+      render action: new
+    end
+  end
+
+  private
+    def list_params
+      params.require(:list).permit(:title).merge(user: current_user)
+    end
 end
