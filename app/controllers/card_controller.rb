880ef6a2c4476457a9bbb7f8d1abf8105a3ed3bd
ywenli <kuota0204@gmail.com>
2020-07-15 14:56:50 +0900

    Add destroy action #14

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,5 +1,5 @@
 class CardController < ApplicationController
-  before_action :set_card, only: %i(show edit update)
+  before_action :set_card, only: %i(show edit update destroy)
 
   def new
     @card = Card.new
@@ -27,6 +27,11 @@ class CardController < ApplicationController
     else
       render action: :edit
     end
+  end
+
+  def destroy
+    @card.destroy
+    redirect_to :root
   end
 
   private
