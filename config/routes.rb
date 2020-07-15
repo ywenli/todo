9e63c086a18628ae5a6c9ad636b63d7eb9a76f3c
ywenli <kuota0204@gmail.com>
2020-07-15 13:28:15 +0900

    Add routing: edit and update #13

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
   resources :list, only: %i(new create edit updte destroy) do
-    resources :card, only: %i(new create show)
+    resources :card, only: %i(new create show edit update)
   end
 end
