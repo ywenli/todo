f897c9a4e8710e5a99a12ebf78ffc2e459ef5f41
ywenli <kuota0204@gmail.com>
2020-07-15 13:33:44 +0900

    Add action: edit and update #13

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,4 +1,5 @@
 class CardController < ApplicationController
+  before_action :set_card, only: %i(show edit update)
 
   def new
     @card = Card.new
@@ -15,12 +16,26 @@ class CardController < ApplicationController
   end
 
   def show
-    @card = Card.find_by(id: params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @card.update_attributes(card_params)
+      redirect_to :root
+    else
+      render action: :edit
+    end
   end
 
   private
     def card_params
       params.require(:card).permit(:title, :memo, :list_id)
+    end
+
+    def set_card
+      @card = Card.find_by(id: params[:id])
     end
 
 end
