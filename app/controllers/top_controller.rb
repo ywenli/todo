04d95a0e485aac2617b7117f448f013de9cee4d5
ywenli <kuota0204@gmail.com>
2020-07-15 23:23:37 +0900

    Add new_guest action in top_controller.rb #19

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -4,4 +4,11 @@ class TopController < ApplicationController
     @lists = List.where(user: current_user).order("created_at ASC")
   end
 
+  def new_guest
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+    sign_in user
+    redirect_to root_path
+  end
 end
