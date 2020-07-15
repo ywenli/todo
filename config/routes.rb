56a54de0695b482e656a60396621668eb17603b5
ywenli <kuota0204@gmail.com>
2020-07-15 18:59:05 +0900

    Add routing #19

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   }
 
   root 'top#index'
+  post '/top/guest_sign_in', to:'top#new_guest'
+
   resources :list, only: %i(new create edit updte destroy) do
     resources :card, only: %i(new create show edit update destroy)
   end
