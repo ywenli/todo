7f855b89fb9fd294bae469ee2d16ed34836b207f
ywenli <kuota0204@gmail.com>
2020-07-15 17:51:12 +0900

    Add routing #11

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   root 'top#index'
   resources :list, only: %i(new create edit updte destroy) do
     resources :card, only: %i(new create show edit update destroy)
