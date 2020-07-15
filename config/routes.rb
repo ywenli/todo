0dfc0f8255673070684cd55483829d6d6192b271
ywenli <kuota0204@gmail.com>
2020-07-15 14:55:20 +0900

    Add routing: destroy #14

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
   resources :list, only: %i(new create edit updte destroy) do
-    resources :card, only: %i(new create show edit update)
+    resources :card, only: %i(new create show edit update destroy)
   end
 end
