28acb057bd9baed26c0371467b6df56b311914d3
ywenli <kuota0204@gmail.com>
2020-07-15 18:05:02 +0900

    Add edit action #11

--- app/controllers/users/unlocks_controller.rb
+++ app/controllers/users/unlocks_controller.rb
@@ -1,21 +1,10 @@
 # frozen_string_literal: true
 
 class Users::UnlocksController < Devise::UnlocksController
-  # GET /resource/unlock/new
-  # def new
-  #   super
-  # end
 
-  # POST /resource/unlock
-  # def create
-  #   super
-  # end
-
-  # GET /resource/unlock?unlock_token=abcdef
-  # def show
-  #   super
-  # end
-
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
   # protected
 
   # The path used after sending unlock password instructions
