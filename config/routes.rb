c940a8569402d26472d4b103456703944e67c0ce
ywenli <kuota0204@gmail.com>
2020-07-15 23:32:45 +0900

    Add routing #19

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy"
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
 
   root 'top#index'
-  post '/top/guest_sign_in', to:'top#new_guest'
 
   resources :list, only: %i(new create edit updte destroy) do
     resources :card, only: %i(new create show edit update destroy)
